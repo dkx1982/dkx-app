6736fd716e486a4434d6917e6603e7f08992053c
dkx1982 <dkx1982@163.com>
2017-07-15 17:41:37 +0800

    welcome ok

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,50 @@
+class Admin::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.delete
+  end
+
+  private
+
+  def product_params
+    params.requrie(:product).promit(:title, :description, :price)
+  end
+end
