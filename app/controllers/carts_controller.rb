d94a90ac7078828abae191a2685554eeb29e0006
dkx1982 <dkx1982@163.com>
2017-07-21 10:37:53 +0800

    add delete all and delete cart_item

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,15 @@
 class CartsController < ApplicationController
+
+  def all_delete
+    current_cart.cart_items.delete_all
+    redirect_to :back
+    flash[:notice] = "以全部清空"
+  end
+
+  def destroy
+    cart_item = current_cart.cart_items.find(params[:id])
+    cart_item.destroy
+    redirect_to :back
+    flash[:notice] = "#{cart_item.product.title}已被删除"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,14 @@ Rails.application.routes.draw do
   resources :products do
     member do
       post :add_to_cart
+
+    end
+
+  end
+  resources :carts do
+    collection do
+      post :all_delete
     end
   end
-  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
